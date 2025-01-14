0baaaaa574f23990adcf9abe0d8dc03712a8b754
bmostowski1996 <bmostowski1996@gmail.com>
2025-01-13 20:19:00 -0500

    added button functionality

---
+++ assets/js/logic.js
@@ -0,0 +1,22 @@
+infos = [
+    'Welcome to New Orleans, LA', 
+    'Welcome to Key West, FL', 
+    'Welcome to New York, NY', 
+    'Welcome to Atlanta, GA', 
+    'Welcome to Washington, D.C.'];
+
+for (let i = 0; i < 5; i++) {
+    let idstr = '#loc' + String(i+1);
+    const location = document.querySelector(idstr);
+
+    const updateInfo = function () {
+        const locInfo = document.querySelector('#loc-info');
+        locInfo.textContent = infos[i];
+    }
+    // updates.push(updateInfo);
+    // locs.push(location);
+
+    location.addEventListener('click', updateInfo);
+}
+
+console.log(locs);
