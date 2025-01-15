df12c4c9200a5be4d60663b5bfb8934811b71a22
Kendrick Wilkerson <Kendrickwilkerson22@gmail.com>
2025-01-14 22:05:41 -0500

    updated lines of code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -32,6 +32,8 @@ for (let i = 0; i < 5; i++) {
 
         location.addEventListener('click', updateInfo);
     }
+
+   const showTextbox = document.querySelector('#show-textbox');    
 }
 
 foo = false;
@@ -81,4 +83,25 @@ window.onload = () => {
 
 
 window.addEventListener('resize', resizeMap);
+
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block"
+  };
+
+  // Function to close the popup
+  function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+  };
+
+  // Add event listeners to the areas
+  document.querySelectorAll('.popup-trigger').forEach(function(area) {
+    area.addEventListener('click', function(e) {
+      e.preventDefault();
+      const popupId = this.getAttribute('data-popup');
+      openPopup(popupId);
+    });
+  });
+
+
+
 
