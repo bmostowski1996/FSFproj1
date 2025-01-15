b4b0800c7f8d865f9e12ba72ef4d2d1c491873da
bmostowski1996 <bmostowski1996@gmail.com>
2025-01-14 20:33:17 -0500

    The map scales with the webpage now

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 infos = [
-    'Welcome to New Orleans, LA', 
     'Welcome to Key West, FL', 
+    'Welcome to New Orleans, LA', 
     'Welcome to New York, NY', 
     'Welcome to Atlanta, GA', 
     'Welcome to Washington, D.C.'];
@@ -36,3 +36,35 @@ const toggleFoo = function() {
 
 toggleFoo();
 // console.log(locs);
+
+const resizeMap = function () {
+    // When the webpage is resized, make sure that the image map coordinates are set properly
+    const img = document.querySelector('#map');
+    const imgMap = document.querySelector('#image-map');
+
+    const originalWidth = 1100;
+    const originalHeight = 703;
+
+    const scaleX = img.clientWidth / originalWidth;
+    const scaleY = img.clientHeight / originalHeight;
+
+    imgMap.querySelectorAll('area').forEach(area => {
+        const coords = area.dataset.originalCoords.split(',').map(Number);
+
+        const scaledCoords = coords.map((coord, i) =>
+            i % 2 === 0 ? coord * scaleX : coord * scaleY
+        );
+        area.coords = scaledCoords.join(',');
+    });
+}
+
+window.onload = () => {
+    document.querySelectorAll('area').forEach(area => {
+        area.dataset.originalCoords = area.coords;
+    });
+    resizeMap();
+};
+
+
+window.addEventListener('resize', resizeMap);
+
